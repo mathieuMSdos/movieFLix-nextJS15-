407ca9b22dec65307020ec3f23cfbbbc896ff04f
msDos <mathieumsdos@gmail.com>
2024-11-27 19:17:43 +0100

    featureNav/activelink

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,15 @@
+"use client"
+
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const NavBar = () => {
+
+  // gestion de la page active
+    const pathname = usePathname()
+
+
   return (
     <nav className="h-full w-full flex">
       {/* le logo */}
@@ -17,10 +25,10 @@ const NavBar = () => {
       {/* les navlink */}
       <ul className="h-full w-full flex justify-center items-center gap-4 font-semibold text-slate-100 ">
         <li>
-          <Link href="/">Accueil</Link>
+          <Link href="/" className={pathname === "/" ? "relative before:content-['] before:absolute before:top-6 before:w-full before:h-1 before:bg-white "  : ""}>Accueil</Link>
         </li>
         <li>
-          <Link href="/about-us">About us</Link>
+          <Link href="/about-us" className={pathname === "/about-us" ? "relative before:content-['] before:absolute before:top-6 before:w-full before:h-1 before:bg-white" : ""}>About us</Link>
         </li>
       </ul>
     </nav>
